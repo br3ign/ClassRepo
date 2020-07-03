043d1a4ced18f0ae6e3f8f668a0b65e0edc7ef50
Alex Samm <alexsamm231@hotmail.com>
2020-07-02 23:20:50 -0300

    Rename Opt2-IISInstall.ps1 to Opt1-IISInstall.ps1

--- lab_scripts/WinServer_Build_Scripts/Opt2-IISInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Optional: Install IIS
-
-# 1. Set server names
-$servers = ('WINDC01')
-
-# 2. Install Feature
-Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -ComputerName $servers -LogPath 'C:\ceh_temp\feature_log.txt'
